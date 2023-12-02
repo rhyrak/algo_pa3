a9f9a6d9df5e84eb4c7088abfebb75ed2b483df1
rhyrak <sgencay88@gmail.com>
2023-12-02 22:59:25 +0300

    fix ID collisions

--- src/RandomStudentGenerator.java
+++ src/RandomStudentGenerator.java
@@ -43,21 +43,46 @@ public class RandomStudentGenerator {
             return null;
         }
 
+        long start, end;
+        start = System.nanoTime();
+        int[][] rands = new int[810][999]; // ~3MB
+        for (int i = 0; i < 999; i++)
+            rands[0][i] = i + 1;
+        for (int i = 1; i < 810; i++)
+            System.arraycopy(rands[0], 0, rands[i], 0, 999);
+        end = System.nanoTime();
+        // ~1ms
+        // System.out.println("[Time] create NNN array: " + (end - start));
+
+        start = System.nanoTime();
         for (int i = 0; i < 81000; i++) {
             int department = i % 9 + 1;
             int faculty = i % 81 + 1;
             int year = 14 + i % 10;
-            int rand = (int) (Math.random() * 999) + 1;
+            // LCM(9,81,10) = 810
+            // i = 0  -> dep: 1 fac: 1 year: 14
+            // i = 810 -> dep: 1 fac: 1 year: 14
+            // dep = year = fac iff i % 810 = 0
+
+            int availables = 999 - i / 810;
+            int randIndex = (int) (Math.random() * availables);
+            int rand = rands[i % 810][randIndex];
+            rands[i % 810][randIndex] = rands[i % 810][availables - 1];
+
             Student student = new Student();
             student.name = fnames.get(i % fnames.size());
             student.lastName = lnames.get(i);
             student.department = String.format("Department #%02d", department);
-            student.faculty = String.format("Department #%02d", faculty);
+            student.faculty = String.format("Faculty #%02d", faculty);
             student.ID = Integer.parseInt(String.format("%d%02d%02d%03d", year, faculty, department, rand));
             generated.add(student);
         }
-
         Collections.shuffle(generated);
+        end = System.nanoTime();
+
+        // ~500ms
+        // System.out.println("[Time] generate db: " + (end - start));
+
         return generated;
     }
 
--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,14 @@
 import java.io.Serializable;
 
-public  class Student implements Serializable {
+public class Student implements Serializable {
     public String name;
     public String lastName;
     public int ID;
     public String department;
     public String faculty;
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %s %s %s", ID, name, lastName, department, faculty);
+    }
 }

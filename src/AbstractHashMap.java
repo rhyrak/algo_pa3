e19e662fea4283c3df3120d078511bedab76b1ad
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-12-06 23:47:20 +0300

    Update AbstractHashMap.java

--- src/AbstractHashMap.java
+++ src/AbstractHashMap.java
@@ -1,33 +1,32 @@
 import java.util.ArrayList;
 
-public abstract class AbstractHashMap<K, V> {
+public abstract class AbstractHashMap {
 
     int capacity;
     int size;
 
-    ArrayList<Node<K, V>> nodes;
+    ArrayList<Student> nodes;
 
     /* Override this in the concrete implementations */
     AbstractHashMap(){}
 
     /* Override this in the concrete implementations */
-    public void insertNode(K key, V value){}
-
-    /* Override this in the concrete implementations */
-    public void deleteNode(K key, V value){}
-
-    /* Override this in the concrete implementations */
-    public V getNode(K key){return null;}
+    public Student getNode(int key){return null;}
 
     public boolean isEmpty(){return size == 0;}
 
+    /* Override this in the concrete implementations */
+    public void createTable() {}
+
+    public void printStudents() {}
+
     /* Override this in the concrete CHAINING implementations */
-    public int hashCode(K key) throws Exception {
+    public int hashCode(int key) throws Exception {
         return super.hashCode();
     }
 
     /* Override this in the concrete OPEN-ADDRESSING implementations */
-    public int hashCode(K key, int i) throws Exception {
+    public int hashCode(int key, int i) throws Exception {
         return super.hashCode();
     }
 }
